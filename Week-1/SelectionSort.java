d66889babdf8a25c7d38264686c0226b6a5bcda0
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-07-15 23:09:50 +0530

    main

---
+++ Week-1/SelectionSort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] numbers = new int[size];
+        System.out.println("Enter " + size + " numbers:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        sort(numbers);
+
+        sc.close();
+    }
+
+    public static void sort(int[] numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = numbers[i];
+            numbers[i] = numbers[minIndex];
+            numbers[minIndex] = temp;
+        }
+
+        System.out.println("The Sorted Array is:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
